05cad7e22c23372e33c58051d5f9053e675ecf77
bora <75948295+Seconb@users.noreply.github.com>
2024-12-16 15:03:14 -0500

    Update RequirementsManager.cs

--- Aimmy2/Other/RequirementsManager.cs
+++ Aimmy2/Other/RequirementsManager.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Windows;
@@ -25,72 +25,5 @@ namespace Other
             return false;
         }
 
-        public static bool IsMemoryIntegrityEnabled() // false if enabled true if disabled, you want it disabled
-        {
-            //credits to Themida
-            string keyPath = @"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforceCodeIntegrity";
-            string valueName = "Enabled";
-            object? value = Registry.GetValue(keyPath, valueName, null);
-            if (value != null && Convert.ToInt32(value) == 1)
-            {
-                new NoticeBar("You have Memory Integrity enabled, please disable it to use Logitech Driver", 7000).Show();
-                return false;
-            }
-            else return true;
-        }
-
-        public static bool CheckForGhub()
-        {
-            try
-            {
-                Process? process = Process.GetProcessesByName("lghub").FirstOrDefault(); //gets the first process named "lghub"
-                if (process == null)
-                {
-                    ShowLGHubNotRunningMessage();
-                    return false;
-                }
-
-                string ghubfilepath = process.MainModule.FileName;
-                if (ghubfilepath == null)
-                {
-                    new NoticeBar($"An error occurred. Run as admin and try again.", 6000).Show();
-                    return false;
-                }
-
-                FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(ghubfilepath);
-
-                if (!versionInfo.ProductVersion.Contains("2021"))
-                {
-                    ShowLGHubImproperInstallMessage();
-                    return false;
-                }
-
-                return true;
-            }
-            catch (AccessViolationException ex)
-            {
-                new NoticeBar($"An error occured: {ex.Message}\nRun as admin and try again.", 6000).Show();
-                return false;
-            }
-        }
-
-        private static void ShowLGHubNotRunningMessage()
-        {
-            if (MessageBox.Show("LG HUB is not running, is it installed?", "Aimmy - LG HUB Mouse Movement", MessageBoxButton.YesNo, MessageBoxImage.Error) == MessageBoxResult.No)
-            {
-                if (MessageBox.Show("Would you like to install it?", "Aimmy - LG HUB Mouse Movement", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
-                {
-                    new LGDownloader().Show();
-                }
-            }
-        }
-
-        private static void ShowLGHubImproperInstallMessage()
-        {
-            if (MessageBox.Show("LG HUB install is improper, would you like to install it?", "Aimmy - LG HUB Mouse Movement", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
-            {
-                new LGDownloader().Show();
-            }
-        }
     }
 }
